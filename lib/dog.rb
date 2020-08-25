362f2441c645f3df565c16407d257a75706135ab
Alexander Clamor <alexander.clamor@gmail.com>
2020-08-25 17:41:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
-# Add your code here
+class Dog
+  @@all = []
+  
+  def initialize
+    self.save
+  end
+  
+  def self.save
+    @@all << self    
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    @@all.each {|dog| puts dog}
+  end
+  
+end
+
+fido = Dog.new
+lassie = Dog.new
+Dog.pring_all
